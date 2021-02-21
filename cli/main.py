50e48e8f3dfe5a51fbf562ec9f142a2a358b135f
Joon Song <Joon7891@gmail.com>
2021-02-20 22:24:01 -0500

    Finished CLI interaction; still need to add more custom options

--- cli/main.py
+++ cli/main.py
@@ -2,42 +2,85 @@ from os import path
 from cryptography_helpers import AESCipher, sha256_string
 from getpass import getpass
 import json
+import click
 
 data_file = 'data.json'
 
-def main():
-    password = ''
+def get_password():
     if not path.exists(data_file):
-        pass_input1, pass_input2 = 1, 2
+        password_input1, password_input2 = 1, 2
 
-        while pass_input1 != pass_input2:
-            pass_input1 = getpass('New Password:')
-            pass_input2 = getpass('Confirm Password:')
+        while password_input1 != password_input2:
+            password_input1 = getpass('New Password:')
+            password_input2 = getpass('Confirm Password:')
 
-            if pass_input1 != pass_input2:
+            if password_input1 != password_input2:
                 print('Passwords did not match, please try again.')
-            
-            password = pass_input1
         
         file = open(data_file, 'w')
-        pass_hash = sha256_string(password)        
         file_data = {
-            'password_hash': pass_hash
+            'password_hash': sha256_string(password_input1)
         }
         file.write(json.dumps(file_data))
+
+        return password_input1
     
+    file = open(data_file, 'r')
+    file_data = json.loads(file.readline())
+
+    password_guess = getpass('Enter Password:')
+    hashed_guess = sha256_string(password_guess)
+
+    if hashed_guess != file_data['password_hash']:
+        raise Exception('Incorrect Password.')
+    
+    return password_guess
+
+def handle_read(password, account):
+    file = open(data_file, 'r')
+    file_data = json.loads(file.readline())
+
+    if account in file_data:
+        encrypted_data = file_data[account]
+        cipher = AESCipher(password)
+        data = cipher.decrypt(encrypted_data)
+
+        print(f'Password for \'{account}\' is: \'{data}\'')
     else:
-        file = open(data_file, 'r')
-        file_data = json.loads(file.readline())
+        print(f'No entry exists for {account}.')
 
-        pass_guess = getpass('Enter Password:')
-        hashed_guess = sha256_string(pass_guess)
+def handle_write(password, account):
+    file = open(data_file, 'r')
+    file_data = json.loads(file.readline())
 
-        if hashed_guess != file_data['password_hash']:
-            print('Incorrect password.')
-        else:
-            print('Correct password.')
+    account_pass1, account_pass2 = 1, 2
+    while account_pass1 != account_pass2:
+        account_pass1 = getpass(f'Enter password for \'{account}\':')
+        account_pass2 = getpass(f'Confirm password for \'{account}\':')
 
+        if account_pass1 != account_pass2:
+            print('Passwords did not match, please try again.')
+
+    cipher = AESCipher(password)
+    file_data[account] = cipher.encrypt(account_pass1)
+
+    file = open(data_file, 'w')
+    file.write(json.dumps(file_data))
+
+@click.command(context_settings=dict(
+    ignore_unknown_options=True,
+))
+@click.argument('access')
+@click.argument('account')
+def main(access, account):
+    if access != '-r' and access != '-w':
+        print('Must specify read/write access via -r or -w flag.')
+        raise Exception('No access flag passed')
+
+    password = get_password()
+
+    if access == '-r': handle_read(password, account)
+    else: handle_write(password, account)
         
 if __name__ == '__main__':
     main()
